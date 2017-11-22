305db0bfe11ba7ea4e6fffaadeb49904e0bf3e1d
Long Huynh Huu <long.upcase@gmail.com>
2017-11-22 19:08:40 +0100

    first commit

---
+++ latex/auto/custom-commands.el
@@ -0,0 +1,58 @@
+(TeX-add-style-hook
+ "custom-commands"
+ (lambda ()
+   (TeX-add-symbols
+    '("inner" 1)
+    '("SPowerset" 1)
+    '("Powerset" 1)
+    '("Set" 2)
+    '("OneOrZero" 1)
+    '("Card" 1)
+    "argmax"
+    "id"
+    "codim"
+    "eps"
+    "defeq"
+    "defequiv"
+    "dist"
+    "sth"
+    "relint"
+    "sign"
+    "For"
+    "ForAll"
+    "ForSome"
+    "With"
+    "meet"
+    "bigmeet"
+    "join"
+    "bigjoin"
+    "AbelianGroups"
+    "AbelianMonoids"
+    "AffineHull"
+    "ConeHull"
+    "ConvexHull"
+    "Edges"
+    "Face"
+    "FaceLattice"
+    "Graph"
+    "NormalCone"
+    "NormalFan"
+    "Perm"
+    "Polytopes"
+    "VirtualPolytopes"
+    "TangentCone"
+    "Vertices"
+    "GeneralisedSupportFunction"
+    "DefCone"
+    "EdgeCone"
+    "FacetCone"
+    "VertexEdgeCone"
+    "VertexCone"
+    "im"
+    "interior"
+    "Span"
+    "End"
+    "refl"
+    "supp"))
+ :latex)
+
